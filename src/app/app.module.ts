2a1bef62413beb4b1930d589b2616ebf2337b0f1
Marcel <me@m4rc3l.de>
2023-10-15 19:46:04 +0200

    added animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import {LoadingComponent} from './core/loading/loading.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {LoadingComponent} from './core/loading/loading.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/zone/zone.component.ts
+++ src/app/pages/zone/zone.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import {share, switchMap} from "rxjs";
+import {Component} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
 import {ZoneService} from "../../api/zone/zone.service";
+import {routingAnimation} from "../../animation/routing.animation";
 
 @Component({
   selector: 'app-zone',
   templateUrl: './zone.component.html',
-  styleUrls: ['./zone.component.scss']
+  styleUrls: ['./zone.component.scss'],
+  animations: [routingAnimation],
 })
 export class ZoneComponent {
 
